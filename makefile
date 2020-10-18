e9d38657544deaf3241bd117eb39fd951c569a27
Anna Zhang <anna@MacBook-Pro-10.local>
2020-10-18 15:59:08 -0400

    changed encode/decode argument, updated make help

--- makefile
+++ makefile
@@ -1,7 +1,9 @@
 help:
+	$(info )
 	$(info Uses vigenere cipher to encode / decode user inputted string)
 	$(info Input as: make run ARGS="encode/decode plaintext/ciphertext keytext")
 	$(info Ciphertext/plaintext shoule be one word)
+	$(info )
 
 run:
 	python3 vigenere.py $(ARGS)
--- vigenere.py
+++ vigenere.py
@@ -1,29 +1,29 @@
 #/usr/local/bin/python3
-import numpy as np
 import sys
 
+print()
+
 if len(sys.argv) < 4:
-   print("format: ARGS=\"encode/decode plaintext/ciphertext key\"")
+   print("Need three arguments. Format: make run ARGS=\"encode/decode plaintext/ciphertext key\"")
    sys.exit()
 
-if sys.argv[1] == "encode":
+if sys.argv[1].lower() == "encode":
     pm = 1
-elif sys.argv[1] == "decode":
+elif sys.argv[1].lower() == "decode":
     pm = -1
 else:
-    print("first argument must be encode/decode")
+    print("first argument must be \"encode\" or \"decode\" (without quotations)")
     sys.exit()
 
 text = sys.argv[2].upper()
 key = sys.argv[3].upper()
 
-print()
-
-def vigenere(text, key, pm):
+def vigenere(text, key):
     keylen = len(key)
     new = ""
     for i in range(len(text)):
         new += chr((ord(text[i]) + pm * ord(key[i % keylen])) % 26 + 65)
     return new
 
-print(vigenere(text, key, pm))
+print(vigenere(text, key))
+print()
